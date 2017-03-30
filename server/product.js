67328f17a8e029a1b0634c886b8248703ca52372
Rohit Bajaniya <rbajaniya@officebrain.com>
2017-03-30 12:46:47 +0530

    product information wrap to data key

--- server/product.js
+++ server/product.js
@@ -6,12 +6,10 @@ Meteor.methods({
     'products.insertCSVData': function(fileID, datas) {
         let _data = [];
         datas.forEach(function(d) {
-            _data.push({
-                fileID: fileID,
-                data: d,
-                owner: Meteor.userId(),
-                username: Meteor.user().username,
-            });
+            d['fileID'] = fileID;
+            d['owner'] = Meteor.userId();
+            d['username'] = Meteor.user().username;
+            _data.push(d);
         });
         return Products.batchInsert(_data);
     }
