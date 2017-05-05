55d63dcc8dad85d651922681e5e65303d902e858
Rohit Bajaniya <rbajaniya@officebrain.com>
2017-05-05 11:46:45 +0530

    view rfq request and reply

--- imports/ui/rfq/rfq.js
+++ imports/ui/rfq/rfq.js
@@ -9,36 +9,71 @@
 
   Template.rfq.events({
       'click .rfq_id': function(event, template) {
-          var currentEl = event.currentTarget;
+          let currentEl = event.currentTarget;
           $('.rfq-view .nav li').removeClass('active');
           $(currentEl).addClass('active');
           let rfqId = $(currentEl).attr('data-id');
           getDiscussion(rfqId, template);
+      },
+      'click #btnReply': function(event, template) {
+          replay(template);
+      },
+      'keypress  #txtReply': function(event, template) {
+          if (event.which === 13) {
+              event.stopPropagation();
+              replay(template);
+              return false;
+          }
       }
   });
 
-  var getDiscussion = function(rfqId, template) {
+  let replay = function(template) {
+      let txtReply = $('#txtReply').val();
+      let activeRfq = template.rfqdiscussion.get();
+      let replyMessage = {
+          from: Meteor.userId(),
+          message: '<p>' + txtReply + '</p>',
+          to: CollCloseOutPromoRFQSent.find({ _id: activeRfq._id }).OwnerId
+      }
+      activeRfq._source.discussion.push(replyMessage);
+      saveReply(activeRfq, template, function() {
+          template.rfqdiscussion.set(activeRfq);
+          $('#txtReply').val('');
+          setTimeout(function() {
+              $('.rfq-messages').scrollTop($('.rfq-messages')[0].scrollHeight);
+          }, 100);
+      })
+  }
+
+
+  let saveReply = function(updatedRfq, template, cb) {
+      let bulkRows = [{ "update": { "_id": updatedRfq._id, "_type": "discussions", "_index": "rfq" } }, { "doc": { "discussion": updatedRfq._source.discussion } }];
+      let bulkRowsString = bulkRows.map(function(row) {
+          return JSON.stringify(row);
+      }).join("\n") + "\n"
+
+      $.ajax({
+          url: Meteor.settings.public.elasticsearch.host + "/_bulk",
+          type: "POST",
+          xhrFields: {
+              withCredentials: true
+          },
+          //dataType: "application/json; charset=utf-8",
+          data: bulkRowsString,
+          dataType: "text",
+          username: Meteor.settings.public.elasticsearch.username, // Most SAP web services require credentials
+          password: Meteor.settings.public.elasticsearch.password,
+          success: function(response) { if (!response.errors) { cb(); } },
+          error: function(xhr, ajaxOptions, thrownError) {},
+      });
+  }
+
+  let getDiscussion = function(rfqId, template) {
       let _rfq = template.rfq.get();
       //console.log('rfqId', rfqId);
       let data = _.find(_rfq, function(d) { return d._id == rfqId });
       //console.log('selecteddata', data);
       template.rfqdiscussion.set(data);
-      //   $.ajax({
-      //       url: "https://7e94c6196993fd6c3d570a3d40e100d1.us-east-1.aws.found.io:9243/rfq/discussions/_search?q=rfqid:" + rfqId,
-      //       type: "GET",
-      //       xhrFields: {
-      //           withCredentials: true
-      //       },
-      //       dataType: "application/json; charset=utf-8",
-      //       username: "elastic", // Most SAP web services require credentials
-      //       password: "SwmLSQli9PvtUNsm63Ce7dpr",
-      //       processData: false,
-      //       contentType: "application/json",
-      //       //data: JSON.stringify(data),
-      //       success: function(response) {
-      //           template.rfqdiscussion.set(response.hits.hits);
-      //       }
-      //   });
   }
 
   Template.rfq.onCreated(function() {
